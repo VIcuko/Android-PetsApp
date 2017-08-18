212788eba90a9e8b0eee673ef87fc8a5c3e36acf
Vicuko <victorc.fernandez@gmail.com>
2017-08-18 18:14:44 +0200

    Update PetContract with pets table columns and values for gender

--- app/src/main/java/com/example/android/pets/data/PetContract.java
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -1,8 +1,26 @@
 package com.example.android.pets.data;
 
+import android.provider.BaseColumns;
+
 /**
  * Created by Vicuko on 18/8/17.
  */
 
-public class PetContract {
+public final class PetContract {
+
+    public static abstract class PetsEntry implements BaseColumns {
+
+        public static final String TABLE_NAME = "pets";
+        public static final String COLUMN_NAME = "_id";
+        public static final String COLUMN_BREED = "breed";
+        public static final String COLUMN_GENDER = "gender";
+        public static final String COLUMN_WEIGHT = "weight";
+
+        /*
+        Possible values for the gender of the pet
+         */
+        public static final int GENDER_UNKNOWN = 0;
+        public static final int GENDER_MALE = 1;
+        public static final int GENDER_FEMALE = 2;
+    }
 }
