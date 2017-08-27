a9b3d6c7631464688f00d2808c6a3ad7422f7caf
Vicuko <victorc.fernandez@gmail.com>
2017-08-27 23:30:57 +0200

    Implement PetCursorAdapters newView and bindView

--- app/src/main/java/com/example/android/pets/PetCursorAdapter.java
+++ app/src/main/java/com/example/android/pets/PetCursorAdapter.java
@@ -2,9 +2,11 @@ package com.example.android.pets;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.TextView;
 
 /**
  * Created by Vicuko on 27/8/17.
@@ -32,8 +34,7 @@ public class PetCursorAdapter extends CursorAdapter{
      */
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        // TODO: Fill out this method and return the list item view (instead of null)
-        return null;
+        return LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
     }
 
     /**
@@ -48,6 +49,17 @@ public class PetCursorAdapter extends CursorAdapter{
      */
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
-        // TODO: Fill out this method
+
+        // Find fields to populate in inflated template
+        TextView petName = (TextView) view.findViewById(R.id.name);
+        TextView petSummary = (TextView) view.findViewById(R.id.summary);
+
+        // Extract properties from cursor
+        String name = cursor.getString(cursor.getColumnIndexOrThrow("name"));
+        String summary = cursor.getString(cursor.getColumnIndexOrThrow("breed"));
+
+        // Populate fields with extracted properties
+        petName.setText(name);
+        petSummary.setText(summary);
     }
 }
