e69b1ba5520c5c9d1b2b71a0a4c93dcfd6ee762f
Vicuko <victorc.fernandez@gmail.com>
2017-08-28 11:29:37 +0200

    Set empty view to petListView in Catalog Activity

--- app/src/main/java/com/example/android/pets/CatalogActivity.java
+++ app/src/main/java/com/example/android/pets/CatalogActivity.java
@@ -128,6 +128,10 @@ public class CatalogActivity extends AppCompatActivity {
         // Find the ListView which will be populated with the pet data
         ListView petListView = (ListView) findViewById(R.id.list_view_pet);
 
+        // Find and set empty view on the ListView, so that it only shows when the list has 0 items.
+        View emptyView = findViewById(R.id.empty_view);
+        petListView.setEmptyView(emptyView);
+
         // Setup an Adapter to create a list item for each row of pet data in the Cursor.
         PetCursorAdapter adapter = new PetCursorAdapter(this, cursor);
 
