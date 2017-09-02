51a86b2dcdbe0b9daa498d8a86c2c2fbef1afc4d
Vicuko <victorc.fernandez@gmail.com>
2017-09-02 09:29:51 +0200

    update databse version

--- app/src/main/java/com/example/android/pets/data/PetDbHelper.java
+++ app/src/main/java/com/example/android/pets/data/PetDbHelper.java
@@ -14,7 +14,7 @@ public class PetDbHelper extends SQLiteOpenHelper {
 
     public static final String LOG_TAG = PetDbHelper.class.getSimpleName();
     // If you change the database schema, you must increment the database version.
-    public static final int DATABASE_VERSION = 1;
+    public static final int DATABASE_VERSION = 2;
 
     public static final String DATABASE_NAME = "shelter.db";
 
